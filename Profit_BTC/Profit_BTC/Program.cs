3c71e3de522c0cc633f10fe57163a483557ea60a
PZapolski <94378698+PZapolski@users.noreply.github.com>
2021-11-15 15:40:52 +0100

    Add files via upload

---
+++ Profit_BTC/Profit_BTC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace btc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, e;
+            Console.WriteLine("Kurs zakupu:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kurs sprzedaży:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ile wkładu?");
+            c = Convert.ToDouble(Console.ReadLine());
+            d = ((c / a) * b) - c;
+            e = ((b / a) * 100) + (-100);
+            Console.WriteLine("Zyska USD: " + d + " USD");
+            Console.WriteLine("Zysk %: " + e + " %");
+            Console.ReadKey();
+        }
+    }
+}
